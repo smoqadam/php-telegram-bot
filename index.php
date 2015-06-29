d1c2bd5fbd7014dfbced2516a5cbfebff88d1013
Saeed <phpro.ir@gmail.com>
2015-06-30 03:36:47 +0430

    fix minor bugs

--- index.php
+++ index.php
@@ -2,24 +2,26 @@
 require_once 'loader.php';
 $tg = new Telegram('68777318:AAFxwqwuIrW9xZ4pi1bM3QJwrHpf1wOYbgQ');
 
-
-while(true){
-	$tg->cmd('test' , function ($args) use ($tg){
-			if($args){
-				$tg->sendMessage( 'سلام علی !! ');
-			}
-		}
-	);
+$tg->cmd('/name' , function ($args) use ($tg){
+		$tg->forwardMessage();
+		$tg->sendMessage("my username is @".$tg->getMe());
+});
 
 
-	$tg->cmd('/help' , function($args) use($tg){
-		var_dump(($args==true));
-		if(!$args){
-			$tg->sendMessage("test [args]\r\nhelp [command]");
-		}
-echo $args;
-		if($args == 'test'){
-			$tg->sendMessage("http://digiato.com/wp-content/uploads/2015/06/screen-shot-2015-06-28-at-10.12.44-am.jpg");
-		}
-	});
-}
+$tg->cmd('/help' , function($args) use($tg){
+
+	if(!$args){
+		$tg->sendMessage("/name 	get my name \r\n/help 	[command]");
+	}
+
+	if($args == 'name'){
+		$tg->sendMessage("get my name");
+	}
+
+	if($args == 'help'){
+		$tg->sendMessage("show this menu");
+	}
+});
+
+
+$tg->run();
--- lib/Telegram.php
+++ lib/Telegram.php
@@ -5,105 +5,187 @@ class Telegram{
 
 	public  $api = 'https://api.telegram.org/bot';
 
-	private  $chat_id ; 
+	private $chat_id ; 
 
-	private  $update_id ; 
+	private $update_id ; 
+
+	private $result ;
+
+	private $commands = [];
 
 	private $available_commands = [
-		    'getMe',
-		    'sendMessage',
-		    'forwardMessage',
-		    'sendPhoto',
-		    'sendAudio',
-		    'sendDocument',
-		    'sendSticker',
-		    'sendVideo',
-		    'sendLocation',
-		    'sendChatAction',
-		    'getUserProfilePhotos',
-		    'getUpdates',
-		    'setWebhook',
+		'getMe',
+		'sendMessage',
+		'forwardMessage',
+		'sendPhoto',
+		'sendAudio',
+		'sendDocument',
+		'sendSticker',
+		'sendVideo',
+		'sendLocation',
+		'sendChatAction',
+		'getUserProfilePhotos',
+		'getUpdates',
+		'setWebhook',
 	];
 
 
 	public function __construct($token){
 		$this->api .= $token;
-	}
-
-
-	public function cmd($cmd , $func)
-	{
-		$result = $this->exec('getUpdates' , ['offset'=>$this->update_id+1,'limit'=>1,'timeout'=>1]);
-		if($result){
-
-			if($result['result'] != null){
-				
-				$result = $result['result'][0];
-				$this->update_id = $result['update_id'];
-				$this->chat_id   = $result['message']['chat']['id'];
-				$reciveMessage   = $result['message']['text'];
-				$args = $this->proccessArgs($reciveMessage);
-				if(is_callable($func) AND $cmd == $args['cmd']){
-					return $func($args['args']);
-				}
-			}else{
-				echo "no new message\r\n";
-			}
-		}
+		$this->message = new stdClass();
 	}
 
 
 	/**
-	* @param msg reviced message 
+	* add new command to the bot
+	* @param $cmd String
+	* @param $func Closure 
 	*/
-	private function proccessArgs($msg )
+	public function cmd($cmd , $func)
+	{
+		$this->commands[$cmd] = $func;
+	}
+
+
+	/**
+	* this method check for recived message(command) and then execute the 
+	* command function
+	*/
+	public function run()
+	{
+
+		$this->result = $this->exec('getUpdates') ;
+		while(true){
+			$update_id = isset($this->result->update_id) ? $this->result->update_id : 1;
+			$result = $this->exec('getUpdates' , ['offset'=> $update_id + 1,'limit'=>1,'timeout'=>1]) ;
+
+			if($result){
+				print_r($result);
+				$this->result = $result;
+				list($cmd , $args) = $this->processMessage($this->result->message->text);
+
+				if(isset($this->commands[$cmd])){
+					if(is_callable($this->commands[$cmd])){
+						echo 		$this->log("command : $cmd ");
+						$func = $this->commands[$cmd];
+						$func($args);
+					}	
+				}
+			}else{
+				echo $this->log("no new message");
+			}
+			// sleep(1);
+		}
+	}
+
+
+   /**
+	* @param String $msg 
+	*/
+	private function processMessage ($msg )
 	{
 		if(!$msg)
 			return null;
-
 		$args = null;
 		$msg = explode(' ',$msg);
-		$cmd = array_shift($msg);
+		$cmd = strtolower(array_shift($msg));
 		if(!empty($msg))
-			$args = implode(' ',$msg);
+			$args = strtolower(implode(' ',$msg));
 
-		return ['cmd'=>trim($cmd),'args'=>trim($args)];
-
+		return [trim($cmd),trim($args)];
 	}
 
 
-
-	public function sendMessage( $text)	
-	{
-		$this->exec('sendMessage',['chat_id'=>$this->chat_id,'text'=>$text]);
-	}
-
-	public function sendImage($test)
-	{
-				$this->exec('sendMessage',['chat_id'=>$this->chat_id,'text'=>'https://www.google.com/images/nav_logo195.png']);
-
-	}
-
-	public function exec($command , $params = [])
+	/**
+	* execute Telegram api commands
+	* @para $command String
+	* @param $params Array
+	*/
+	private function exec($command , $params = [])
 	{
 		if(in_array($command, $this->available_commands)	){
-				$params = http_build_query($params);
-				return json_decode($this->curl_get_contents($this->api.'/'.$command.'?'.$params),true);
+			$params = http_build_query($params);
+			// convert json to array then get the last messages info 
+			$output = json_decode($this->curl_get_contents($this->api.'/'.$command.'?'.$params),true);
+
+			$result = [] ;
+			// we need to last message information
+			if($output) 
+				$result = end(end($output));
+
+			if(!empty($result))
+			{
+				// convert to object 
+				return json_decode(json_encode($result));
+			}
 		}else{
 			echo 'command not found';
 		}
 	}
 
 
-	function curl_get_contents($url)
+   /**
+    *  get telegram api content with curl
+    */
+   private function curl_get_contents($url)
+   {
+   	$ch = curl_init($url);
+   	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+   	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+   	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+   	curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+   	$data = curl_exec($ch);
+   	curl_close($ch);
+   	return $data;
+   }
+
+
+   private function log($log)
+   {
+   	return "\r\n$log";
+   }
+
+
+	/**
+	* send message
+	* @param String 
+	*/
+	public function sendMessage( $text)	
 	{
-		  $ch = curl_init($url);
-		  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-		  curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-		  curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-		  curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-		  $data = curl_exec($ch);
-		  curl_close($ch);
-		  return $data;
+		$this->exec('sendMessage',[
+			'chat_id'=>$this->result->message->chat->id,
+			'text'=>$text
+			]);
 	}
+
+	public function sendImage($test)
+	{
+		$this->exec('sendMessage',['chat_id'=>$this->result->message->chat->id,'text'=>'']);
+	}
+
+
+	public function getMe()
+	{
+		return $this->exec('getMe');
+	}
+
+
+	public function getMe2()
+	{
+		return $this->exec('getMe');
+	}
+
+
+	public function forwardMessage()
+	{
+		return $this->exec('forwardMessage',[
+			'chat_id'     =>$this->result->message->chat->id,
+			'from_chat_id'=>$this->result->message->from->id ,
+			'message_id'  =>$this->result->message->message_id,
+		 ]);
+	}
+
+
+
+
 }
