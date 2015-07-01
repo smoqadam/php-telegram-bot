5775d16da54369f52fefa31a8bf94e6c90a923a1
Saeed <phpro.ir@gmail.com>
2015-07-02 02:36:04 +0430

    fix composer.json

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 require 'vendor/autoload.php';
 
 
-$tg = new Telegram('68777318:AAFxwqwuIrW9xZ4pi1bM3QJwrHpf1wOYbgQ');
+$tg = new Smoqadam\Telegram('68777318:AAFxwqwuIrW9xZ4pi1bM3QJwrHpf1wOYbgQ');
 
 $tg->cmd('\/name:<<[a-zA-Z]{0,}>>', function ($args) use ($tg){
 		$tg->sendMessage("my username is @".$args);
