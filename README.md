beada486600ec23bfce6ffb2b65b6b3dda649e22
Saeed <phpro.ir@gmail.com>
2015-07-01 15:18:39 +0430

    minor improvments

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # php-telegram-bot
 a wrapper class for telegram bot api
+
+#this class has not been compeleted yet!!
+
+how to use :
+
+```php
+require_once 'lib/Telegram.php';
+$tg = new Telegram('TELEGRAM_API');
+
+$tg->cmd('\/number: <<:num>>' , function($args) use($tg){
+	$tg->sendMessage("your number is : ".$args); 
+});
+
+
+$tg->cmd('Hello',function ($args) use ($tg){
+			$tg->sendMessage("Hello World!");
+});
+
+$tg->run();
+
+```
+
+now when you send `/number 123` in telegram bot page , bot will answer to you `your number is 123`
+
+you can set argument in regex between << and >> 
--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php
-require_once 'lib/Telegram.php';
+//require_once 'lib/Telegram.php';
+
+require 'vendor/autoload.php';
+
+
 $tg = new Telegram('68777318:AAFxwqwuIrW9xZ4pi1bM3QJwrHpf1wOYbgQ');
 
 $tg->cmd('\/name:<<[a-zA-Z]{0,}>>', function ($args) use ($tg){
@@ -8,14 +12,17 @@ $tg->cmd('\/name:<<[a-zA-Z]{0,}>>', function ($args) use ($tg){
 
 
 $tg->cmd('\/number: <<:num>>' , function($args) use($tg){
-
-		$tg->sendMessage("your number is : ".$args); 
-
+	$tg->sendMessage("your number is : ".$args); 
 });
 
 
 $tg->cmd('Hello',function ($args) use ($tg){
-			$tg->sendMessage("Hello World!");
+	$image = fopen('e:\\logo.php.png', 'r');
+	// $image = file_get_contents('e:\\logo.php.png');
+    $images = json_decode(file_get_contents('http://ajax.googleapis.com/ajax/services/search/images?v=1.0&q=' . urlencode($description) . '&rsz=8'), true)['responseData']['results'];
+    $image = fopen($images[array_rand($images)]['unescapedUrl'], 'r');
+	
+	$tg->sendPhoto($tg->result->message->chat->id , '@'.$image);
 });
 
 $tg->run();
