af98425937920d65905ea96d5a59deb091e032d4
Saeed Moghadam <smoqadam@users.noreply.github.com>
2015-07-04 04:02:03 +0430

    fix minor bugs

--- index.php
+++ index.php
@@ -3,22 +3,24 @@
 
 require 'vendor/autoload.php';
 
+use Smoqadam\Telegram;
 
-$tg = new Smoqadam\Telegram('68777318:AAFxwqwuIrW9xZ4pi1bM3QJwrHpf1wOYbgQ');
+$tg = new Telegram('API_TOKEN');
 
 $tg->cmd('\/name:<<[a-zA-Z]{0,}>>', function ($args) use ($tg){
-		$tg->sendMessage($tg->result->message->chat->id , "my username is @".$args);
+		$tg->sendMessage("my username is @".$args , $tg->getChatId());
 });
 
 
 $tg->cmd('\/number: <<:num>>' , function($args) use($tg){
-	$tg->sendMessage($tg->result->message->chat->id , "your number is : ".$args); 
+	$tg->sendMessage("your number is : ".$args , $tg->getChatId()); 
 });
 
 
-$tg->cmd('Hello',function ($args) use ($tg){
-	$image = 'logo.php.png';
-	$tg->sendSticker($tg->result->message->chat->id , $image);
+$tg->cmd('Hello',function () use ($tg){
+	$tg->sendChatAction(Telegram::ACTION_TYPING);
+	$image = 'urltoqrcode.png';
+	$tg->sendPhoto($image);
 });
 
 $tg->run();
