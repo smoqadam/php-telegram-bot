e6b1540ed925937532ad4d80512940325145f6e1
Saeed <phpro.ir@gmail.com>
2015-07-05 02:02:31 +0430

    fix index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ require 'vendor/autoload.php';
 
 use Smoqadam\Telegram;
 
-$tg = new Telegram('68777318:AAFxwqwuIrW9xZ4pi1bM3QJwrHpf1wOYbgQ');
+$tg = new Telegram('API_TOKEN');
 
 $tg->cmd('\/name:<<[a-zA-Z]{0,}>>', function ($args) use ($tg){
 		$tg->sendMessage("my username is @".$args , $tg->getChatId());
