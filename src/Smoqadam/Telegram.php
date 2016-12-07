c8b459ebcc291c54908acd9a71583c0852904ffa
Saeed Moqadam <smoqadam@users.noreply.github.com>
2016-12-07 15:45:52 +0330

    Revert "Sync Develop branch"

--- src/Smoqadam/Telegram.php
+++ src/Smoqadam/Telegram.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * A wrapper class for telegram bot api
- *
+ * 
  * @home page : https://github.com/smoqadam/php-telegram-bot
  * @author : Saeed Moqadam <phpro.ir@gmail.com>
  * @licence : MIT Licence
@@ -9,19 +9,20 @@
  */
 namespace Smoqadam;
 
-class Telegram {
 
-  const PARSE_MARKDOWN = 'Markdown';
-  const PARSE_HTML     = 'HTML';
+class Telegram
+{
 
-  const ACTION_TYPING         = 'typing';
-  const ACTION_UPLOAD_PHOTO   = 'upload_photo';
-  const ACTION_RECORD_VIDEO   = 'record_video';
-  const ACTION_UPLOAD_VIDEO   = 'upload_video';
-  const ACTION_RECORD_AUDIO   = 'record_audio';
-  const ACTION_UPLOAD_AUDIO   = 'upload_audio';
-  const ACTION_UPLOAD_DOC     = 'upload_document';
-  const ACTION_FIND_LOCATION  = 'find_location';
+
+    const ACTION_TYPING = 'typing';
+    const ACTION_UPLOAD_PHOTO = 'upload_photo';
+    const ACTION_RECORD_VIDEO = 'record_video';
+    const ACTION_UPLOAD_VIDEO = 'upload_video';
+    const ACTION_RECORD_AUDIO = 'record_audio';
+    const ACTION_UPLOAD_AUDIO = 'upload_audio';
+    const ACTION_UPLOAD_DOC = 'upload_document';
+    const ACTION_FIND_LOCATION = 'find_location';
+
 
     public $api = 'https://api.telegram.org/bot';
 
@@ -31,27 +32,14 @@ class Telegram {
      */
     public $result;
 
-     /**
-      * @name State of bot
-     * state of bot
-     * @var object
-     */
-    public $state;
-
     /**
-     * commands in regex and callback
+     * commands in regex
      * @var array
      */
     private $commands = [];
 
     /**
-     * InlineQuery in regex and callback
-     * @var array
-     */
-    private $inlines = [];
-
-    /**
-     * callbacks
+     * callbacks for commands
      * @var array
      */
     private $callbacks = [];
@@ -72,7 +60,6 @@ class Telegram {
         'sendLocation',
         'sendChatAction',
         'getUserProfilePhotos',
-        'answerInlineQuery',
         'getUpdates',
         'setWebhook',
     ];
@@ -91,165 +78,203 @@ class Telegram {
      *
      * @param String $token Telegram api token , taken by botfather
      */
-    public function __construct($token) {
+    public function __construct($token)
+    {
+
         $this->api .= $token;
+
     }
+
 
     /**
      * add new command to the bot
      * @param String $cmd
      * @param \Closure $func
      */
-    public function cmd($cmd, $func) {
-      $this->commands[] = new Trigger($cmd, $func);
+    public function cmd($cmd, $func)
+    {
+        $this->commands[] = $cmd;
+        $this->callbacks[] = $func;
     }
 
-    /**
-     * add new InlineQuery to the bot
-     * @param String $cmd
-     * @param \Closure $func
-     */
-    public function inlineQuery($cmd, $func) {
-      $this->inlines[] = new Trigger($cmd, $func);
-    }
 
     /**
-     * this method check for received payload(command, inlinequery and so on) and
-     * then execute the correct function
+     * this method check for recived message(command) and then execute the
+     * command function
      *
      * @param bool $sleep
      */
-    public function run($sleep = false) {
+    public function run($sleep = false)
+    {
+
         $result = $this->getUpdates();
         while (true) {
+
             $update_id = isset($result->update_id) ? $result->update_id : 1;
             $result = $this->getUpdates($update_id + 1);
 
-            $this->processPayload($result);
-
+            $this->processMessage($result);
+            
             if ($sleep !== false)
                 sleep($sleep);
         }
     }
 
 	/**
-	* this method used for setWebhook sended payload
+	* this method used for setWebhook sended message
 	*/
-    public function process($payload) {
-    	$result = $this->convertToObject($payload, true);
+    public function process($message)
+    {
+    		$result = $this->convertToObject($message, true);
 
-      return $this->processPayload($result);
+            $update_id = isset($result->update_id) ? $result->update_id : 1;
+
+            return $this->processMessage($result);
+
+            
     }
 
-    private function processPayload($result) {
+
+    private function processMessage($result)
+    {
     	if ($result) {
-            try {
-                $this->result = $result;
+                try {
+                    $this->result = $result;
 
-                // now i select the right triggers for payload received by Telegram
-                if( isset($this->result->message->text) ) {
-                  $payload = $this->result->message->text;
-                  $triggers = $this->commands;
-                } elseif ( isset($this->result->inline_query) ) {
-                  $payload = $this->result->inline_query->query;
-                  $triggers = $this->inlines;
-                } else {
-                  throw new \Exception("Error Processing Request", 1);
-                }
+                    // message recived by user
+                    $recived_command = $this->result->message->text;
 
-                $args = null;
+                    $args = null;
 
-                foreach ($triggers as &$trigger) {
-                    // replace public patterns to regex pattern
-                    $searchs = array_keys($this->patterns);
-                    $replaces = array_values($this->patterns);
-                    $pattern = str_replace($searchs, $replaces, $trigger->pattern);
+                    $pos = 0;
+                    foreach ($this->commands as $pattern) {
 
-                    //find args pattern
-                    $args = $this->getArgs($pattern, $payload);
+                        // replace public patterns to regex pattern
+                        $searchs = array_keys($this->patterns);
+                        $replaces = array_values($this->patterns);
+                        $pattern = str_replace($searchs, $replaces, $pattern);
 
-                    $pattern = '/^' . $pattern . '/i';
+                        //find args pattern
+                        $args = $this->getArgs($pattern, $recived_command);
 
-                    preg_match($pattern, $payload, $matches);
+                        $pattern = '/^' . $pattern . '/i';
 
-                    if (isset($matches[0])) {
-                        $func = $trigger->callback;
-                        call_user_func($func, $args);
+                        preg_match($pattern, $recived_command, $matches);
+
+                        if (isset($matches[0])) {
+
+                            $func = $this->callbacks[$pos];
+
+                            call_user_func($func, $args);
+
+                        }
+
+                        $pos++;
+
                     }
+                } catch (\Exception $e) {
+                    echo "\r\n Exception :: " . $e->getMessage();
                 }
-            } catch (\Exception $e) {
-              error_log($e->getMessage());
-              echo "\r\n Exception :: " . $e->getMessage();
+            } else {
+                echo "\r\nNo new message\r\n";
             }
-        } else {
-            echo "\r\nNo new message\r\n";
-        }
     }
-
     /**
      * get arguments part in regex
      * @param $pattern
-     * @param $payload
+     * @param $recived_command
      * @return mixed|null
      */
-    private function getArgs(&$pattern, $payload) {
+    private function getArgs(&$pattern, $recived_command)
+    {
+
         $args = null;
         // if command has argument
         if (preg_match('/<<.*>>/', $pattern, $matches)) {
 
             $args_pattern = $matches[0];
             //remove << and >> from patterns
+
             $tmp_args_pattern = str_replace(['<<', '>>'], ['(', ')'], $pattern);
 
             //if args set
-            if (preg_match('/' . $tmp_args_pattern . '/i', $payload, $matches)) {
+            if (preg_match('/' . $tmp_args_pattern . '/i', $recived_command, $matches)) {
                 //remove first element
                 array_shift($matches);
+
                 if (isset($matches[0])) {
+
                     //set args
                     $args = array_shift($matches);
 
                     //remove args pattern from main pattern
                     $pattern = str_replace($args_pattern, '', $pattern);
+
                 }
             }
         }
         return $args;
     }
 
+
     /**
      * execute telegram api commands
      * @param $command
      * @param array $params
      */
-    private function exec($command, $params = []) {
+    private function exec($command, $params = [])
+    {
         if (in_array($command, $this->available_commands)) {
-          // convert json to array then get the last messages info
-          $output = json_decode($this->curl_get_contents($this->api . '/' . $command, $params), true);
 
+
+            // convert json to array then get the last messages info
+            $output = json_decode($this->curl_get_contents($this->api . '/' . $command, $params), true);
         	return $this->convertToObject($output);
+
+
         } else {
+
             echo 'command not found';
+
         }
     }
 
-    private function convertToObject($jsonObject , $webhook = false) {
-      if( ! $webhook) {
-        if ($jsonObject['ok']) {
-          //error_log(print_r($jsonObject, true));
 
-          // remove unwanted array elements
-          $output = end($jsonObject);
+    private function convertToObject($jsonObject , $webhook = false)
+    {
+        
+        if(!$webhook){
+          if ($jsonObject['ok']) {
 
-          $result = is_array($output) ? end($output) : $output;
-          if ( ! empty($result)) {
-              // convert to object
-              return json_decode(json_encode($result));
-          }
+                // remove unwanted array elements
+                $output = end($jsonObject);
+
+                $result = is_array($output) ? end($output) : $output;
+
+                if (!empty($result)) {
+                    // convert to object
+                    return json_decode(json_encode($result));
+                }
+            }
+            
+        }else{
+            if ($jsonObject['message']) {
+
+                    return json_decode(json_encode($jsonObject)); 
+                // remove unwanted array elements
+                $output = end($jsonObject);
+
+                $result = is_array($output) ? end($output) : $output;
+
+                if (!empty($result)) {
+                    // convert to object
+                    print_r($result);
+                    return json_decode(json_encode($result));
+                }
+            }
         }
-      } else {
-        return json_decode(json_encode($jsonObject));
-      }
+            
+            
     }
 
     /**
@@ -258,14 +283,21 @@ class Telegram {
      * @param $params
      * @return mixed
      */
-    private function curl_get_contents($url, $params) {
+    private function curl_get_contents($url, $params)
+    {
+
         $ch = curl_init();
 
         curl_setopt($ch, CURLOPT_URL, $url);
+
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
         curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+
         curl_setopt($ch, CURLOPT_POST, count($params));
+
         curl_setopt($ch, CURLOPT_POSTFIELDS, $params);
 
         $result = curl_exec($ch);
@@ -273,6 +305,7 @@ class Telegram {
         curl_close($ch);
 
         return $result;
+
     }
 
     /**
@@ -280,21 +313,12 @@ class Telegram {
      * @param null $chat_id
      * @return int
      */
-    public function getChatId($chat_id = null) {
-      try {
+    public function getChatId($chat_id = null)
+    {
         if ($chat_id)
-          return $chat_id;
+            return $chat_id;
 
-        if( isset($this->result->message) ) {
-          return $this->result->message->chat->id;
-        } elseif ( isset($this->result->inline_query) ) {
-          return $this->result->inline_query->from->id;
-        } else {
-          throw new \Exception("Error Processing Request", 1);
-        }
-      } catch (\Exception $e) {
-        error_log($e->getMessage());
-      }
+        return $this->result->message->chat->id;
     }
 
     /**
@@ -302,13 +326,15 @@ class Telegram {
      * @param int $limit
      * @param int $timeout
      */
-    public function getUpdates($offset = null, $limit = 1, $timeout = 1) {
+    public function getUpdates($offset = null, $limit = 1, $timeout = 1)
+    {
         return $this->exec('getUpdates', [
             'offset' => $offset,
             'limit' => $limit,
             'timeout' => $timeout
         ]);
     }
+
 
     /**
      * send message
@@ -318,8 +344,10 @@ class Telegram {
      * @param null $reply_to_message_id
      * @param null $reply_markup
      */
-    public function sendMessage($text, $chat_id = null, $disable_web_page_preview = false, $reply_to_message_id = null, $reply_markup = null) {
-        $this->sendChatAction(self::ACTION_TYPING, $chat_id);
+    public function sendMessage($text, $chat_id, $disable_web_page_preview = false, $reply_to_message_id = null, $reply_markup = null)
+    {
+
+        $this->sendChatAction(self::ACTION_TYPING,$chat_id);
         return $this->exec('sendMessage', [
             'chat_id' => $this->getChatId($chat_id),
             'text' => $text,
@@ -329,19 +357,23 @@ class Telegram {
         ]);
     }
 
+
     /**
      * Get me
      */
-    public function getMe() {
+    public function getMe()
+    {
         return $this->exec('getMe');
     }
+
 
     /**
      * @param $from_id
      * @param $message_id
      * @param null $chat_id
      */
-    public function forwardMessage($from_id, $message_id, $chat_id = null) {
+    public function forwardMessage($from_id, $message_id, $chat_id = null)
+    {
         return $this->exec('forwardMessage', [
             'chat_id' => $this->getChatId($chat_id),
             'from_chat_id' => $from_id,
@@ -356,16 +388,19 @@ class Telegram {
      * @param null $reply_to_message_id
      * @param null $reply_markup
      */
-    public function sendPhoto($photo, $chat_id = null, $caption = null, $reply_to_message_id = null, $reply_markup = null) {
+    public function sendPhoto($photo, $chat_id = null, $caption = null, $reply_to_message_id = null, $reply_markup = null)
+    {
+
         $res = $this->exec('sendPhoto', [
             'chat_id' => $this->getChatId($chat_id),
-            'photo' => $photo,
+            'photo' => '@' . $photo,
             'caption' => $caption,
             'reply_to_message_id' => $reply_to_message_id,
             'reply_markup' => $reply_markup
         ]);
 
         return $res;
+
     }
 
     /**
@@ -374,10 +409,11 @@ class Telegram {
      * @param null $reply_to_message_id
      * @param null $reply_markup
      */
-    public function sendVideo($video, $chat_id = null, $reply_to_message_id = null, $reply_markup = null) {
+    public function sendVideo($video, $chat_id = null, $reply_to_message_id = null, $reply_markup = null)
+    {
         $res = $this->exec('sendVideo', [
             'chat_id' => $this->getChatId($chat_id),
-            'video' => $video,
+            'video' => '@' . $video,
             'reply_to_message_id' => $reply_to_message_id,
             'reply_markup' => $reply_markup
         ]);
@@ -392,10 +428,11 @@ class Telegram {
      * @param null $reply_to_message_id
      * @param null $reply_markup
      */
-    public function sendSticker($sticker, $chat_id = null, $reply_to_message_id = null, $reply_markup = null) {
+    public function sendSticker($sticker, $chat_id = null, $reply_to_message_id = null, $reply_markup = null)
+    {
         $res = $this->exec('sendSticker', [
             'chat_id' => $this->getChatId($chat_id),
-            'sticker' => $sticker,
+            'sticker' => '@' . $sticker,
             'reply_to_message_id' => $reply_to_message_id,
             'reply_markup' => $reply_markup
         ]);
@@ -411,7 +448,8 @@ class Telegram {
      * @param null $reply_to_message_id
      * @param null $reply_markup
      */
-    public function sendLocation($latitude, $longitude, $chat_id = null, $reply_to_message_id = null, $reply_markup = null) {
+    public function sendLocation($latitude, $longitude, $chat_id = null, $reply_to_message_id = null, $reply_markup = null)
+    {
         $res = $this->exec('sendLocation', [
             'chat_id' => $this->getChatId($chat_id),
             'latitude' => $latitude,
@@ -429,10 +467,11 @@ class Telegram {
      * @param null $reply_to_message_id
      * @param null $reply_markup
      */
-    public function sendDocument($document, $chat_id = null, $reply_to_message_id = null, $reply_markup = null) {
+    public function sendDocument($document, $chat_id = null, $reply_to_message_id = null, $reply_markup = null)
+    {
         $res = $this->exec('sendDocument', [
             'chat_id' => $this->getChatId($chat_id),
-            'document' => $document,
+            'document' => '@' . $document,
             'reply_to_message_id' => $reply_to_message_id,
             'reply_markup' => $reply_markup
         ]);
@@ -440,10 +479,11 @@ class Telegram {
         return $res;
     }
 
-    public function sendAudio($audio, $chat_id = null, $reply_to_message_id = null, $reply_markup = null) {
+    public function sendAudio($audio, $chat_id = null, $reply_to_message_id = null, $reply_markup = null)
+    {
         $res = $this->exec('sendAudio', [
             'chat_id' => $this->getChatId($chat_id),
-            'audio' => $audio,
+            'audio' => '@' . $audio,
             'reply_to_message_id' => $reply_to_message_id,
             'reply_markup' => $reply_markup
         ]);
@@ -456,7 +496,8 @@ class Telegram {
      * @param $action
      * @param null $chat_id
      */
-    public function sendChatAction($action, $chat_id = null) {
+    public function sendChatAction($action, $chat_id = null)
+    {
         $res = $this->exec('sendChatAction', [
             'chat_id' => $this->getChatId($chat_id),
             'action' => $action
@@ -470,7 +511,8 @@ class Telegram {
      * @param null $offset
      * @param null $limit
      */
-    public function getUserProfilePhotos($user_id, $offset = null, $limit = null) {
+    public function getUserProfilePhotos($user_id, $offset = null, $limit = null)
+    {
         $res = $this->exec('getUserProfilePhotos', [
             'user_id' => $user_id,
             'offset' => $offset,
@@ -480,30 +522,17 @@ class Telegram {
         return $res;
     }
 
-
-    public function answerInlineQuery($inline_query_id, $results, $cache_time = 0, $is_personal = false, $next_offset = '', $switch_pm_text = '', $switch_pm_parameter = '') {
-      $res = $this->exec('answerInlineQuery', [
-        'inline_query_id' => $inline_query_id,
-        'results' => json_encode($results),
-        'cache_time' => $cache_time,
-        'is_personal' => $is_personal,
-        'next_offset' => $next_offset,
-        'switch_pm_text' => $switch_pm_text,
-        'switch_pm_parameter' => $switch_pm_parameter
-      ]);
-
-      return $res;
-    }
-
     /**
      * @param $url
      */
-    public function setWebhook($url) {
+    public function setWebhook($url)
+    {
         $res = $this->exec('setWebhook', [
             'url' => $url
         ]);
 
         return $res;
     }
+
 
 }
